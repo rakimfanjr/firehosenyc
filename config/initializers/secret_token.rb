adc403457906607f329269b594045f07e4ddff2e
Ian White <ian@ian-VirtualBox.(none)>
2013-10-19 12:35:29 -0400

    creating the landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'adf0d77a182b08df8e8214ab63773abc29c6a124d31c5ea3fca9f6faa6c92a4b9a9a4d89b730004fac24812f0424e60357f61a348e869b3e137c29ec3683ff69'
