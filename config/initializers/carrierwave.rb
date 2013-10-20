45e8031f38a4e231248cbd50746f61665f89433f
Ian White <ian@ian-VirtualBox.(none)>
2013-10-20 15:04:57 -0400

    added user oauth via device gen

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['AWS_ACCESS_KEY'],
 			:aws_secret_access_key => ENV['AWS_SECRET_KEY']
-		}
- 
+	
+	} 
 		config.fog_directory = ENV['AWS_BUCKET']
 	end
 end
