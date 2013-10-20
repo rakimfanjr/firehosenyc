8310bb0d7de127619b787a9f04d260d13e5f71ca
Ian White <ian@ian-VirtualBox.(none)>
2013-10-20 15:17:44 -0400

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,7 @@ Testapp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-config.action_mailer.default_url_options = { :host +> 'firehoseweekendNYC_IWW'}
+config.action_mailer.default_url_options = { 
+  :host => 'firehoseweekendNYC_IWW.herokuapp.com'
+}
 end
